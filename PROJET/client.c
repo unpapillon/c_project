87955ece2d18f7a567b62489e9be0242ad89a5e5
unpapillon <radetclem.pro@gmail.com>
2022-04-03 15:05:49 +0200

    chat working TODO:authentification

--- PROJET/client.c
+++ PROJET/client.c
@@ -11,14 +11,17 @@
 
 #define LENGTH 2048
 
-char name[30];
+struct arg_struct {
+    int sockfd;
+    char name[30];
+};
 
-
-void send_msg_handler(void *arg) {
+void send_msg_handler(void *arguments) {
     char message[LENGTH] = {};
     char buffer[LENGTH + 32] = {};
-    int *sockvalue = (int *) arg;
-    int sockfd = *sockvalue;
+    struct arg_struct *args = arguments;
+    // int *sockvalue = (int *) arguments;
+    // int sockfd = *sockvalue;
 
     while(1) {
         fgets(message, LENGTH, stdin);
@@ -26,23 +29,23 @@ void send_msg_handler(void *arg) {
         if (strcmp(message, "exit") == 0) {
                 break;
         } else {
-        sprintf(buffer, "%s: %s\n", name, message);
-        send(sockfd, buffer, strlen(buffer), 0);
+        sprintf(buffer, "%s: %s\n", args -> name, message);
+        send(args -> sockfd, buffer, strlen(buffer), 0);
         }
-
+        //clean messages and buffer
         bzero(message, LENGTH);
         bzero(buffer, LENGTH + 32);
     }
-
 }
 
-void recv_msg_handler(void *arg) {
+void recv_msg_handler(void *arguments) {
 	char message[LENGTH] = {};
-    int *sockvalue = (int *) arg;
-    int sockfd = *sockvalue;
+    struct arg_struct *args = arguments;
+    // int *sockvalue = (int *) arg;
+    // int sockfd = *sockvalue;
 
     while (1) {
-        int receive = recv(sockfd, message, LENGTH, 0);
+        int receive = recv(args -> sockfd, message, LENGTH, 0);
         if (receive > 0) {
         printf("%s", message);
         } else if (receive == 0) {
@@ -57,11 +60,9 @@ void recv_msg_handler(void *arg) {
 
 int main(int argc, char **argv){
 
-    int sockfd = 0;
-    char name[32];
+    struct arg_struct args;
+    args.sockfd = 0;
     struct sockaddr_in server_addr;
-
-
 
 	if(argc != 2){
 		printf("Usage: %s <port>\n", argv[0]);
@@ -72,47 +73,52 @@ int main(int argc, char **argv){
 	int port = atoi(argv[1]);
 
 	printf("Please enter your name: ");
-    fgets(name, 32, stdin);
+    fgets(args.name, 32, stdin);
 
 
-	if (strlen(name) > 32 || strlen(name) < 2){
+	if (strlen(args.name) > 32 || strlen(args.name) < 2){
 		printf("Name must be less than 30 and more than 2 characters.\n");
 		return EXIT_FAILURE;
 	}
 
 
 	/* Socket settings */
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	args.sockfd = socket(AF_INET, SOCK_STREAM, 0);
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr(ip);
     server_addr.sin_port = htons(port);
 
 
   // Connect to Server
-  int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  int err = connect(args.sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
   if (err == -1) {
 		printf("ERROR while connecting\n");
 		return EXIT_FAILURE;
 	}
 
 	// Send name
-	send(sockfd, name, 32, 0);
+	send(args.sockfd, args.name, 32, 0);
 
 	printf("=== WELCOME TO THE CHATROOM ===\n");
 
-	pthread_t send_msg_thread;
-  if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, (void*)&sockfd) != 0){
+
+    pthread_t recv_msg_thread;
+    pthread_t send_msg_thread;
+	
+    if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, (void*)&args) != 0){
+		printf("ERROR while using thread for receive messagesn");
+		return EXIT_FAILURE;
+	}
+    if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, (void*)&args) != 0){
 		printf("ERROR while using thread for sending message\n");
     return EXIT_FAILURE;
 	}
 
-	pthread_t recv_msg_thread;
-  if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, (void*)&sockfd) != 0){
-		printf("ERROR while using thread for receive messagesn");
-		return EXIT_FAILURE;
-	}
 
-	close(sockfd);
+    while(1){
+    
+  
+    }
 
 	return EXIT_SUCCESS;
 }
